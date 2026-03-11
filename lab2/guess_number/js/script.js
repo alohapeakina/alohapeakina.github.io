b3ae45e770f269e16657c8e3543f3b4f7fa4403d
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-03-10 22:58:13 -0700

    Added game logic

--- lab2/guess_number/js/script.js
+++ lab2/guess_number/js/script.js
@@ -1,1 +1,57 @@
-// alert("running external JS code!")
+//Global variables
+let randomNumber;
+let attempts = 0;
+
+//Event listeners
+document.querySelector("#guessBtn").addEventListener("click",checkGuess);
+
+initializeGame();
+
+function initializeGame() {
+    randomNumber = Math.floor(Math.random() * 99) + 1;
+    console.log("randomNumber: " + randomNumber);
+    
+    //hiding the Reset button
+    document.querySelector("#resetBtn").style.display="none";
+    
+    //adding focus to textbox
+    document.querySelector("#playerGuess").focus();
+    
+}
+
+function checkGuess() {
+    let feedback = document.querySelector("#feedback");
+    feedback.textContent = "";
+    let guess = document.querySelector("#playerGuess").value;
+    console.log("Player guess: " + guess);
+    if (guess < 1 || guess > 99) {
+        feedback.textContent = "Enter a number between 1 and 99";
+        feedback.style.color = "red";
+        return;
+    }
+    attempts++;
+    console.log("Attempts: " + attempts);
+    feedback.style.color = "orange";
+    if (guess == randomNumber) {
+        feedback.textContent = "You guessed it! You Won!";
+        feedback.style.color = "darkgreen";
+    } else {
+        document.querySelector("#guesses").textContent += guess + " ";
+        if (attempts == 7) {
+            feedback.textContent = "Sorry, you lost!";
+            feedback.style.color = "red";
+            gameOver();
+        } else if (guess > randomNumber) {
+            feedback.textContent = "Guess was high";
+        } else {
+            feedback.textContent = "Guess was low";
+        }
+    }
+}
+
+function gameOver() {
+    let guessBtn = document.querySelector("#guessBtn");
+    let resetBtn = document.querySelector("#resetBtn");
+    guessBtn.style.display = "none";
+    resetBtn.style.display = "inline";
+}
