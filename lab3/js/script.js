7607f0b3b942cc3af1faec1530f21d398d92f9bb
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-03-15 07:42:51 -0700

    Fixed error with suggestedPwd being wrong kind of quotations. Added checks for password length. TODO: Validate password and retry password are the same

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,7 +1,13 @@
+//Global variables
+let usernameAvailable = false;
+
 //Event Listeners
 document.querySelector("#zip").addEventListener("change",displayCity);
 document.querySelector("#state").addEventListener("change",displayCounties);
 document.querySelector("#username").addEventListener("change", checkUsername);
+document.querySelector("#signupForm").addEventListener("submit",function(event) {
+    validateForm(event);
+});
 
 //Functions
 
@@ -38,12 +44,49 @@ async function checkUsername() {
     let response = await fetch(url);
     let data = await response.json();
     let usernameError = document.querySelector("#usernameError");
+
     if (data.available) {
         usernameError.innerHTML = "Username available!";
         usernameError.style.color = "green";
+        usernameAvailable = true;
     }
     else {
         usernameError.innerHTML = "Username taken";
         usernameError.style.color = "red";
+        usernameAvailable = false;
+    }
+}
+
+//Validating form data
+function validateForm(e) {
+    let isValid = true;
+    let username = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
+    console.log("Password value is: " + password);
+
+    let usernameError = document.querySelector("#usernameError");
+    let passwordError = document.querySelector("#suggestedPwd");
+
+    // Clear previous messages
+    usernameError.textContent = "";
+    passwordError.textContent = "";
+
+    if (username.length === 0) {
+        usernameError.textContent = "Username Required!";
+        usernameError.style.color = "red";
+        isValid = false;
+    } else if (!usernameAvailable) {
+        usernameError.textContent = "Username is taken!";
+        usernameError.style.color = "red";
+         isValid = false;
+    }
+
+    if (password.length < 6) {
+        passwordError.textContent = "Password must be at least 6 characters";
+        isValid = false;
+    }
+
+    if (!isValid) {
+        e.preventDefault();
     }
 }
