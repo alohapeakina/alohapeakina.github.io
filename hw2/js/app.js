0327c67db8cf782b1a36ba92bbc81e90a7f2f2d7
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-03-15 02:31:17 -0700

    Simplified isFormValid and refactored grading (but still broken)

--- hw2/js/app.js
+++ hw2/js/app.js
@@ -135,7 +135,7 @@ function loadProgress() {
         for (const option of elements) {
 
             if (option.type === "radio" || option.type === "checkbox") {
-                if (Array.isArray(val)) {
+                if (Array.isArray(val)) { // Logic for handling multiple choice checkboxes
                     option.checked = val.includes(option.value);
                 } else {
                     option.checked = (option.value === val);
@@ -145,31 +145,15 @@ function loadProgress() {
             }
 
         }
-
-
     }
-
-    // if (savedResponses) {
-    //     Object.keys(savedResponses).forEach(question => {
-    //         const val = savedResponses[question];
-    //         const input = document.querySelectorAll(`[name="${question}"]`);
-
-    //         input.forEach(input => {
-    //             if (Array.isArray(val)) {
-    //                 if (val.includes(input.value)) input.checked = true;
-    //             } else {
-    //                 if (input.value === val) input.checked = true;
-    //             }
-    //         });
-    //     });
-    // }
 }
 
+// Removes progress from local storage and clears any text input
 function resetQuiz() {
-    // Removes progress from local storage and clears any text input
     localStorage.removeItem("quiz_progress");
     quizForm.reset();
 
+    // Clears any feedback from individual questions
     const questionCount = document.querySelectorAll(".card-body").length;
     for (let i=1; i<= questionCount; i++) {
         const feedback = document.querySelector(`#q${i}Feedback`);
@@ -184,11 +168,10 @@ function resetQuiz() {
         }
     }
 
+    // Resets overall feedback and results section
     validationFdbk.innerHTML = "";
     validationFdbk.className = "";
-
     results.classList.add("d-none");
-
     score = 0;
 
 }
@@ -203,14 +186,14 @@ function isFormValid() {
     for (let i=1; i<=questionCount.length; i++) {
         let isAnswered = false;
 
-        // Finds all questions via name
-        const input = document.querySelectorAll(`[name="q${i}"]`);
+        // Gets input for each question
+        const input = document.querySelector(`[name="q${i}"]`);
 
         // Verifies presence of response according to input type
-        if (input[0].type === "radio" || input[0].type === "checkbox") {
+        if (input.type === "radio" || input.type === "checkbox") {
             isAnswered = document.querySelector(`input[name="q${i}"]:checked`) !== null;
         } else {
-            isAnswered = input[0].value.trim() !== "";
+            isAnswered = input.value.trim() !== "";
         }
 
         if (!isAnswered) {
@@ -224,6 +207,7 @@ function isFormValid() {
         return false;
     }
 
+    // Extra safety to clear feedback if everything is valid
     validationFdbk.innerHTML = "";
     validationFdbk.className = "";
 
@@ -269,6 +253,29 @@ function wrongAnswer(index) {
     }
 }
 
+// Generalized function to determine if an individual answer is correct
+function isCorrect(userValue, correctValue) {
+    if (Array.isArray(correctValue)) { // Checks if the answer requires multiple selections
+        
+        if (Array.isArray(userValue)) { // Checks if user provided multiple selections
+            if (userValue.length !== correctValue.length) { // If length of arrays don't match, answer can't be correct
+                return false;
+            }
+
+            for (const val of userValue) {
+                if (!correctValue.includes(val)) { // User included a value that wasn't found in answer key
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return correctValue.includes(userValue); // Verifies user provided correct answer if there are multiple correct options
+    }
+
+    return userValue === correctValue;
+}
+
 function gradeQuiz() {
     console.log("Grading quiz");
     score = 0;
@@ -281,94 +288,123 @@ function gradeQuiz() {
 
     // ========== GRADING QUESTIONS ==========
 
+    for (let i=0; i < quizKeys.length; i++) {
+
+        const qName = quizKeys[i];
+        const qNumber = i + 1; // User-facing question numbers are offset from quiz number index
+        const config = QUIZ_SETUP[qName];
+
+        let userResponse;
+        const sampleInput = document.querySelector(`[name=${qName}"]`);
+
+        if (sampleInput.type === "radio") {
+            const checked = document.querySelector(`input[name="${qName}"]:checked`);
+            userResponse = checked ? checked.value : "";
+        } else if (sampleInput.type === "checkbox") {
+            const checkedBoxes = document.querySelectorAll(`input[name=${qName}"]:checked`);
+            userResponse = [];
+            for (const cb of checkedBoxes) {
+                userResponse.push(cb.value);
+            }
+        } else {
+            userResponse = sampleInput.value.toLowerCase().trim();
+        }
+
+        if (isCorrect(userResponse.config.answer)) {
+            rightAnswer(qNumber);
+        } else {
+            wrongAnswer(qNumber);
+        }
+    }
+
     // Grading Q1
-    let q1Response = document.querySelector("input[name=q1]:checked").value;
-    console.log("Q1 Response is: " + q1Response);
-    if (q1Response == "southdakota") {
-        rightAnswer(1);
-    } else {
-        wrongAnswer(1);
-    }
+    // let q1Response = document.querySelector("input[name=q1]:checked").value;
+    // console.log("Q1 Response is: " + q1Response);
+    // if (q1Response == "southdakota") {
+    //     rightAnswer(1);
+    // } else {
+    //     wrongAnswer(1);
+    // }
 
-    // Grading Q2
-    let q2Response = document.querySelector("input[name=q2]:checked").value;
-    console.log("Q2 Response is: " + q2Response);
-    if (q2Response == "arizona") {
-        rightAnswer(2);
-    } else {
-        wrongAnswer(2);
-    }
+    // // Grading Q2
+    // let q2Response = document.querySelector("input[name=q2]:checked").value;
+    // console.log("Q2 Response is: " + q2Response);
+    // if (q2Response == "arizona") {
+    //     rightAnswer(2);
+    // } else {
+    //     wrongAnswer(2);
+    // }
 
-    // Grading Q3
-    let q3Response = document.querySelector("input[name=q3]:checked").value;
-    console.log("Q3 Response is: " + q3Response);
-    if (q3Response == "missouri") {
-        rightAnswer(3);
-    } else {
-        wrongAnswer(3);
-    }
+    // // Grading Q3
+    // let q3Response = document.querySelector("input[name=q3]:checked").value;
+    // console.log("Q3 Response is: " + q3Response);
+    // if (q3Response == "missouri") {
+    //     rightAnswer(3);
+    // } else {
+    //     wrongAnswer(3);
+    // }
 
-    // Grading Q4
-    let q4Response = document.querySelector("input[name=q4]:checked").value;
-    console.log("Q4 Response is: " + q4Response);
-    if (q4Response == "superior") {
-        rightAnswer(4);
-    } else {
-        wrongAnswer(4);
-    }
+    // // Grading Q4
+    // let q4Response = document.querySelector("input[name=q4]:checked").value;
+    // console.log("Q4 Response is: " + q4Response);
+    // if (q4Response == "superior") {
+    //     rightAnswer(4);
+    // } else {
+    //     wrongAnswer(4);
+    // }
 
-    // Grading Q5
-    let q5Response = document.querySelector("input[name=q5]:checked").value;
-    console.log("Q5 Response is: " + q5Response);
-    if (q5Response == "kauai") {
-        rightAnswer(5);
-    } else {
-        wrongAnswer(5);
-    }
+    // // Grading Q5
+    // let q5Response = document.querySelector("input[name=q5]:checked").value;
+    // console.log("Q5 Response is: " + q5Response);
+    // if (q5Response == "kauai") {
+    //     rightAnswer(5);
+    // } else {
+    //     wrongAnswer(5);
+    // }
 
-    // Grading Q6
-    let q6Response = document.querySelector("input[name=q6]:checked").value;
-    console.log("Q6 Response is: " + q6Response);
-    if (q6Response == "false") {
-        rightAnswer(6)
-    } else {
-        wrongAnswer(6);
-    }
+    // // Grading Q6
+    // let q6Response = document.querySelector("input[name=q6]:checked").value;
+    // console.log("Q6 Response is: " + q6Response);
+    // if (q6Response == "false") {
+    //     rightAnswer(6)
+    // } else {
+    //     wrongAnswer(6);
+    // }
 
-    // Grading Q7
-    let q7Response = document.querySelectorAll("input[name=q7]:checked");
-    console.log("Q7 Responses are: " + Array.from(q7Response).map(response => response.value));
-    if (document.querySelector("#q7LosAngeles").checked && document.querySelector("#q7Sacramento").checked &&
-        document.querySelector("#q7SanFrancisco").checked && !document.querySelector("#q7SanDiego").checked) {
-        rightAnswer(7);
-    } else {
-        wrongAnswer(7);
-    }
+    // // Grading Q7
+    // let q7Response = document.querySelectorAll("input[name=q7]:checked");
+    // console.log("Q7 Responses are: " + Array.from(q7Response).map(response => response.value));
+    // if (document.querySelector("#q7LosAngeles").checked && document.querySelector("#q7Sacramento").checked &&
+    //     document.querySelector("#q7SanFrancisco").checked && !document.querySelector("#q7SanDiego").checked) {
+    //     rightAnswer(7);
+    // } else {
+    //     wrongAnswer(7);
+    // }
 
-    // Grading Q8
-    let q8Response = document.querySelector("#q8a").value;
-    console.log("Q8 Response is: " + q8Response);
-    if (q8Response == "ak") {
-        rightAnswer(8);
-    } else {
-        wrongAnswer(8);
-    }
+    // // Grading Q8
+    // let q8Response = document.querySelector("#q8a").value;
+    // console.log("Q8 Response is: " + q8Response);
+    // if (q8Response == "ak") {
+    //     rightAnswer(8);
+    // } else {
+    //     wrongAnswer(8);
+    // }
 
-    let q9Response = document.querySelector("#q9a").value.toLowerCase();
-    console.log("Q9 Response is: " + q9Response);
-    if (q9Response == "delaware" || q9Response == "de") {
-        rightAnswer(9);
-    } else {
-        wrongAnswer(9);
-    }
+    // let q9Response = document.querySelector("#q9a").value.toLowerCase();
+    // console.log("Q9 Response is: " + q9Response);
+    // if (q9Response == "delaware" || q9Response == "de") {
+    //     rightAnswer(9);
+    // } else {
+    //     wrongAnswer(9);
+    // }
 
-    let q10Response = document.querySelector("#q10a").value;
-    console.log("Q10 Response is: " + q10Response);
-    if (q10Response == "6") {
-        rightAnswer(10);
-    } else {
-        wrongAnswer(10);
-    }
+    // let q10Response = document.querySelector("#q10a").value;
+    // console.log("Q10 Response is: " + q10Response);
+    // if (q10Response == "6") {
+    //     rightAnswer(10);
+    // } else {
+    //     wrongAnswer(10);
+    // }
 
     // ========== END OF GRADING QUESTIONS ==========
 
