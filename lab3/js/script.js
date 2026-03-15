fb0b9e1611dca98c5c8c6033229e63cfb2d7c99f
alohapeakina <111400610+alohapeakina@users.noreply.github.com>
2026-03-15 07:00:17 -0700

    Completed Lab3 Lesson 3 - added lat/long and county selection

--- lab3/js/script.js
+++ lab3/js/script.js
@@ -1,5 +1,6 @@
 //Event Listeners
 document.querySelector("#zip").addEventListener("change",displayCity);
+document.querySelector("#state").addEventListener("change",displayCounties);
 
 //Functions
 
@@ -11,4 +12,20 @@ async function displayCity() {
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
+    document.querySelector("#city").innerHTML = data.city;
+    document.querySelector("#latitude").innerHTML = data.latitude;
+    document.querySelector("#longitude").innerHTML = data.longitude;
+}
+
+//Displaying counties from Web API based on the two-letter abbreviation of a state
+async function displayCounties() {
+    let state = document.querySelector("#state").value;
+    let url = `https://csumb.space/api/countyListAPI.php?state=${state}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    let countyList = document.querySelector("#county");
+    countyList.innerHTML ="<option>Select County</option>";
+    for (let i=0; i < data.length; i++) {
+        countyList.innerHTML += `<option> ${data[i].county} </option>`;
+    }
 }
